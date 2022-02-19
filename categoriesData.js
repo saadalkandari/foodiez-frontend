876ee8a08669b3899109bd92ecc3eae8455b2822
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-02-19 19:44:50 +0300

    staged new files

---
+++ categoriesData.js
@@ -0,0 +1,16 @@
+const categories = [
+  {
+    id: 1,
+    title: "Breakfast",
+  },
+  {
+    id: 2,
+    title: "Lunch",
+  },
+  {
+    id: 3,
+    title: "Dinner",
+  },
+];
+
+export default categories;
