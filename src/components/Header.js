9df273d931b211a4766ccd6d2ac748e118f57491
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-02-21 17:26:20 +0300

    HeaderCss

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <header className="bg-dark py-5 recipeBG">
-        {/* hero */}
-        <div className="container px-4 px-lg-5 my-5 ">
-          <div className=" hometitle">
-            <h1 className="display-4 fw-bolder titlez">RECIPES</h1>
-            <p className=" sloganz">recipes made simpler</p>
-          </div>
-        </div>
+    <div className="Header-container">
+      <header className="  recipeBG">
+        <h1 className="titlez">RECIPES</h1>
+        <p className=" sloganz">recipes made simpler</p>
       </header>
     </div>
   );
